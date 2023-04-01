b796a2514a5481c694f59b158e9a5471942ead8f
vasilijpetecuk <frostpv@gmail.com>
2023-04-01 21:31:59 +0300

    [BE][TSK1] - git init

---
+++ src/main/java/com/example/songsstorage/SongsStorageApplication.java
@@ -0,0 +1,13 @@
+package com.example.songsstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongsStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongsStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/songsstorage/SongsStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.songsstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongsStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
